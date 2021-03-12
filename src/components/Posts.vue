b8d74a92ab87acb9985566d0c7f48663c0085f17
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2021-03-12 18:01:18 +0900

    add crud function

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,20 +1,31 @@
 <template>
   <h2>投稿する</h2>
+  <h2>{{ posts }}</h2>
   <ul>
     <li v-for="post in posts" :key='post.id'>
+      <input type="text" v-model="post.name" />
+      <textarea v-model="post.comment"></textarea>
       <p>{{ post.name }}</p>
       <p>{{ post.comment }}</p>
       <input type="button" value="編集" @click="update(post)"/>
       <input type="button" value="削除" @click="remove(post)" />
     </li>
   </ul>
+  <input type="text" v-model="new_post.name">
+  <textarea v-model="new_post.comment"></textarea>
+  <input type="button" value="追加" @click="create">
 </template>
 
 <script>
 export default {
   data() {
     return {
-      posts: []
+      posts: [],
+      new_post: {
+        id: 0,
+        name: null,
+        comment: null
+      }
     }
   },
 
@@ -31,18 +42,46 @@ export default {
     },
 
     // 追加
-    add() {
-
+    create() {
+      fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          id: this.new_post.id + 1,
+          name: this.new_post.name,
+          comment: this.new_post.comment
+        }),
+        headers: new Headers({ 'Content-type' : 'application/json' })
+      })
+        .then( () => {
+          this.posts.push({
+            id: this.new_post.id,
+            name: this.new_post.name,
+            comment: this.new_post.comment
+          })
+          this.new_post = []
+        })
     },
 
     // 編集
-    update() {
-
+    update(post) {
+      fetch(`http://localhost:3000/posts/${post.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          name: post.name,
+          comment: post.comment
+        }),
+        headers: new Headers({ 'Content-type' : 'application/json' })
+      })
     },
 
     // 削除
-    remove() {
-
+    remove(post) {
+      fetch(`http://localhost:3000/posts/${post.id}`, {
+        method: 'DELETE'
+      })
+        .then( () => {
+          this.posts = this.posts.filter(item => item !== post)
+        })
     }
   }
 }
